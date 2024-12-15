9a5e10cd315f166ca16587f93799ff01b84d9fe0
mbelk059 <mbelk818@gmail.com>
2024-12-15 18:36:26 -0500

    fix chat overflow in nav

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -8,73 +8,73 @@ import { NavLink } from 'react-router-dom';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Logo from "../assets/images/logo.png";
+import Container from 'react-bootstrap/Container';
 
 class NavBarComponent extends React.Component {
-	state: any;
+    state: any;
 
-	constructor(props: any) {
-		super(props);
+    constructor(props: any) {
+        super(props);
 
-		this.state = {
-			expanded: false
-		};
+        this.state = {
+            expanded: false
+        };
 
-		this.toggle = this.toggle.bind(this);
-		this.collapse = this.collapse.bind(this);
-	}
+        this.toggle = this.toggle.bind(this);
+        this.collapse = this.collapse.bind(this);
+    }
 
-	toggle() {
-		this.setState({ expanded: !this.state.expanded });
-	}
+    toggle() {
+        this.setState({ expanded: !this.state.expanded });
+    }
 
-	collapse() {
-		this.setState({ expanded: false });
-	}
+    collapse() {
+        this.setState({ expanded: false });
+    }
 
-	render() {
-		return (
-			<>
-				<div
-					id="navbar-hidden-overlay"
-					style={{
-						display: this.state.expanded ? 'block' : 'none'
-					}}
-					onClick={this.collapse}
-				/>
-				<Navbar
-					id="navbar-main"
-					expanded={this.state.expanded}
-					className="navbar-dark"
-					expand="xl"
-					>
-					<Navbar.Brand id="brand" className="mt-2">
-					</Navbar.Brand>
-					<Navbar.Toggle
-						aria-controls="basic-navbar-nav"
-						onClick={this.toggle}
-						style={{ color: 'white', borderColor: 'transparent' }}
-					/>
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="mr-auto ml-auto">
-						{ROUTES_WITH_TITLES.map((routeItem, index) => (
-							<NavLink
-							key={`route-path-link-${index}`}
-							to={Array.isArray(routeItem.path) ? routeItem.path[0] : routeItem.path}
-							onClick={this.collapse}
-							exact
-							style={{ alignSelf: 'center' }}
-							>
-							{routeItem.title}
-							</NavLink>
-						))}
-						</Nav>
-					</Navbar.Collapse>
-					</Navbar>
-
-			</>
-		);
-	}
+    render() {
+        return (
+            <>
+                <div
+                    id="navbar-hidden-overlay"
+                    style={{
+                        display: this.state.expanded ? 'block' : 'none'
+                    }}
+                    onClick={this.collapse}
+                />
+                <Navbar
+                    id="navbar-main"
+                    expanded={this.state.expanded}
+                    className="navbar-dark"
+                    expand="xl"
+                >
+                    <Container fluid>
+                        <Navbar.Toggle
+                            aria-controls="basic-navbar-nav"
+                            onClick={this.toggle}
+                            style={{ color: 'white', borderColor: 'transparent' }}
+                        />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto ml-auto">
+                                {ROUTES_WITH_TITLES.map((routeItem, index) => (
+                                    <NavLink
+                                        key={`route-path-link-${index}`}
+                                        to={Array.isArray(routeItem.path) ? routeItem.path[0] : routeItem.path}
+                                        onClick={this.collapse}
+                                        exact
+                                        className="nav-link"
+                                        style={{ alignSelf: 'center' }}
+                                    >
+                                        {routeItem.title}
+                                    </NavLink>
+                                ))}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
+        );
+    }
 }
 
 export default NavBarComponent;
