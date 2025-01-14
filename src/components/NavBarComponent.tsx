4af67e5aa0fe5dce22aac24ca60a4fa1d8409c5b
mbelk059 <mbelk818@gmail.com>
2025-01-14 17:59:41 -0500

    navbar mobile fix

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -11,56 +11,46 @@ import smallLogo from '../assets/small_logo.png';
 
 const NavBarComponent = () => {
     const [expanded, setExpanded] = useState(false);
-    const isNavBarVisible = true;
-
-    if (!isNavBarVisible) {
-        return null;
-    }
 
     return (
-        <div>
-            <Navbar
-                id="navbar-main"
-                className="navbar-dark"
-                expanded={expanded}
-                expand="xl"  // Collapse at xl breakpoint (1200px)
-            >
-                <Container fluid className="px-3">
-                    <NavLink to="/" className="logo-link position-absolute">
-                        <img
-                            src={smallLogo}
-                            alt="Logo"
-                            className="logo"
-                        />
-                    </NavLink>
+        <Navbar
+            id="navbar-main"
+            className={`navbar-dark ${expanded ? 'expanded' : ''}`}
+            expanded={expanded}
+            expand="xl"
+            fixed="top"
+        >
+            <Container fluid className="px-3">
+                <NavLink to="/" className="logo-link">
+                    <img src={smallLogo} alt="Logo" className="logo" />
+                </NavLink>
 
-                    {/* The toggle button for hamburger menu */}
-                    <Navbar.Toggle
-                        aria-controls="basic-navbar-nav"
-                        onClick={() => setExpanded(!expanded)}
-                        className="hamburger-toggle"
-                    >
-                        <Menu size={35} className="hamburger-icon" />
-                    </Navbar.Toggle>
+                {/* Hamburger Toggle */}
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    onClick={() => setExpanded(!expanded)}
+                    className="hamburger-toggle"
+                >
+                    <Menu size={35} className="hamburger-icon" />
+                </Navbar.Toggle>
 
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto">
-                            {ROUTES_WITH_TITLES.map((routeItem, index) => (
-                                <NavLink
-                                    key={`route-path-link-${index}`}
-                                    to={Array.isArray(routeItem.path) ? routeItem.path[0] : routeItem.path}
-                                    exact
-                                    className="nav-link"
-                                    onClick={() => setExpanded(false)}
-                                >
-                                    {routeItem.title}
-                                </NavLink>
-                            ))}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        {ROUTES_WITH_TITLES.map((routeItem, index) => (
+                            <NavLink
+                                key={`route-path-link-${index}`}
+                                to={Array.isArray(routeItem.path) ? routeItem.path[0] : routeItem.path}
+                                exact
+                                className="nav-link"
+                                onClick={() => setExpanded(false)}
+                            >
+                                {routeItem.title}
+                            </NavLink>
+                        ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
