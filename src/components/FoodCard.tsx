75349be43585fcc09639609aa940df5e8957512e
Miuponn <kellygao@live.ca>
2025-01-17 15:54:48 -0500

    skeleton loader added

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import './FoodCard.css';
 
 interface FoodCardProps {
@@ -10,33 +12,46 @@ interface FoodCardProps {
 }
 
 const FoodCard: React.FC<FoodCardProps> = ({ mealType, restaurant, time, items, imageSrc }) => {
+    const [isImageLoading, setIsImageLoading] = useState(true);
+
+    useEffect(() => {
+        if (imageSrc) {
+            const img = new Image();
+            img.src = imageSrc;
+            img.onload = () => setIsImageLoading(false);
+        } else {
+            setIsImageLoading(false);
+        }
+    }, [imageSrc]);
+
     return (
         <div className={`food-card ${mealType}`}>
-            {/* meal Type header & time aligned */}
+            {/* Meal Type Header & Time Aligned */}
             <div className={`food-card-header ${mealType}`}>
                 <span>{mealType.charAt(0).toUpperCase() + mealType.slice(1)}</span>
                 <span className="food-card-time">{time}</span>
             </div>
 
-            {/* restaurant name */}
+            {/* Restaurant Name */}
             <h3 className={`food-card-title ${mealType}`}>{restaurant}</h3>
 
-            {/* description & image */}
+            {/* Description & Image */}
             <div className="food-card-content">
                 <div className="food-card-description">
                     {items.map(([category, description], index) => (
                         <div className="food-card-item" key={index}>
-                             <span className="item-category">{category}:</span> <span className="item-description">{description}</span>
+                            <span className="item-category">{category}:</span>{' '}
+                            <span className="item-description">{description}</span>
                         </div>
                     ))}
                 </div>
 
-                {/* image with fixed size & scaling */}
+                {/* Image with Skeleton Loader */}
                 <div className="food-card-image">
-                    {imageSrc ? (
-                        <img src={imageSrc} alt={restaurant} />
+                    {isImageLoading ? (
+                        <Skeleton height={180} width={280} />
                     ) : (
-                        <div className="skeleton-loader"></div>
+                        imageSrc && <img src={imageSrc} alt={restaurant} />
                     )}
                 </div>
             </div>
